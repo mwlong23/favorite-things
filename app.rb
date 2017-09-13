b2e43a6c5b0294909aa9c68771e139a438514695
Kelsey Langlois and Mitchell Long <kelsey@>
2017-09-13 09:11:25 -0700

    Add sinatra files and dynamic routing.

---
+++ app.rb
@@ -0,0 +1,23 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/item')
+require('pry')
+
+get('/') do
+  @list = Item.all
+  erb(:list)
+end
+
+post('/') do
+  name = params["name"]
+  item = Item.new(name)
+  item.save
+  @list = Item.all
+  erb(:list)
+end
+
+get('/items/:id') do
+  @item = Item.find(params[:id])
+  erb(:item)
+end
--- lib/item.rb
+++ lib/item.rb
@@ -3,10 +3,12 @@
 class Item
   @@list = []
 
+  attr_reader :id
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @id = @@list.length + 1
   end
 
   def self.all
@@ -19,5 +21,14 @@ class Item
 
   def self.clear
     @@list = []
+  end
+
+  def self.find(id)
+    item_id = id.to_i
+    @@list.each do |item|
+      if item.id == item_id
+        return item
+      end
+    end
   end
 end
